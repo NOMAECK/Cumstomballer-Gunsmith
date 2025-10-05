69f970ec36be34b3c002057ddd96dafec157d3b0
NOMAECK <nomaeck@gmail.com>
2025-10-05 15:07:36 +0200

    Delete Customballer-Gunsmith directory

--- Customballer-Gunsmith/script/Run-Customballer-Gunsmith.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if not "%1"=="min" (
-    start /min "" cmd /c "%~f0" min
-    exit /b
-)
-set "SCRIPT_DIR=%~dp0"
-powershell.exe -ExecutionPolicy Bypass -NoProfile -File "%SCRIPT_DIR%Script.ps1"
-exit /b
