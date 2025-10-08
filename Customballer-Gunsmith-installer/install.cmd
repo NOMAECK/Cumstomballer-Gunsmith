40d47f834b312673346bb99e731c811aa7182d07
NOMAECK <nomaeck@gmail.com>
2025-10-08 12:15:55 +0200

    Customballer Gunsmith installer
    changed language to english

--- Customballer-Gunsmith-installer/install.cmd
+++ Customballer-Gunsmith-installer/install.cmd
@@ -6,19 +6,19 @@ REM ================================
 REM Pfad zum PowerShell-Skript
 set "PS_SCRIPT=%~dp0script\install.ps1"
 
-echo Prüfe, ob "%PS_SCRIPT%" blockiert ist...
+echo check if "%PS_SCRIPT%" is blocked...
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-  "try { $path = '%PS_SCRIPT%'; $zone = Get-Item -LiteralPath $path -ErrorAction Stop | Get-Item -Stream 'Zone.Identifier' -ErrorAction SilentlyContinue; if ($zone) { Write-Host 'Datei ist blockiert – entblocke...'; Unblock-File -Path $path; Write-Host 'Datei erfolgreich entblockt.' } else { Write-Host 'Datei ist nicht blockiert.' } } catch { Write-Host 'Fehler beim Prüfen oder Entblocken:'; Write-Host $_.Exception.Message }"
+  "try { $path = '%PS_SCRIPT%'; $zone = Get-Item -LiteralPath $path -ErrorAction Stop | Get-Item -Stream 'Zone.Identifier' -ErrorAction SilentlyContinue; if ($zone) { Write-Host 'File is blocked, unblock...'; Unblock-File -Path $path; Write-Host 'File successfull unblocked.' } else { Write-Host 'File is not blocked' } } catch { Write-Host 'Error:'; Write-Host $_.Exception.Message }"
 
 if errorlevel 1 (
   echo.
-  echo [FEHLER] beim Entblocken.
+  echo Error while unblocking.
   pause
 )
 
 echo.
-echo Starte PowerShell-Installer...
+echo Start PowerShell-Installer...
 powershell -NoProfile -ExecutionPolicy Bypass -File "%PS_SCRIPT%"
 
 REM Datei selbst löschen (2 Sekunden warten, dann löschen)
