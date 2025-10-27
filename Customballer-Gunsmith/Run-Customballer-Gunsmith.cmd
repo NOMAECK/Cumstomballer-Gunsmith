c78e7e15a9187765e639482ada835380d07b9a58
NOMAECK <nomaeck@gmail.com>
2025-10-27 21:28:00 +0100

    Add files via upload

--- Customballer-Gunsmith/Run-Customballer-Gunsmith.cmd
+++ Customballer-Gunsmith/Run-Customballer-Gunsmith.cmd
@@ -33,17 +33,17 @@ echo.
 echo.
 echo.
 echo.
-echo    Check all .ps1 in "%SCRIPT_DIR%script%" if blocked...
+echo Check all .ps1 in "%SCRIPT_DIR%script%" if blocked...
 timeout /t 2 /nobreak >nul
 echo.
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-  "try { $base = '%SCRIPT_DIR%script'; Get-ChildItem -LiteralPath $base -Recurse -Filter '*.ps1' -File -ErrorAction SilentlyContinue | ForEach-Object { $p=$_.FullName; $zone = Get-Item -LiteralPath $p -ErrorAction Stop | Get-Item -Stream 'Zone.Identifier' -ErrorAction SilentlyContinue; if ($zone) { Write-Host '   unblock:' $p; Start-Sleep -Seconds 1; Unblock-File -LiteralPath $p; Write-Host '   successful:' $p; Start-Sleep -Seconds 1 } else { Write-Host '   not blocked:' $p; Start-Sleep -Seconds 1 } } } catch { Write-Host '   error while unblocking:' $_.Exception.Message; Start-Sleep -Seconds 1; exit 1 }"
+  "try { $base = '%SCRIPT_DIR%script'; Get-ChildItem -LiteralPath $base -Recurse -Filter '*.ps1' -File -ErrorAction SilentlyContinue | ForEach-Object { $p=$_.FullName; $zone = Get-Item -LiteralPath $p -ErrorAction Stop | Get-Item -Stream 'Zone.Identifier' -ErrorAction SilentlyContinue; if ($zone) { Write-Host 'unblock:' $p; Start-Sleep -Seconds 1; Unblock-File -LiteralPath $p; Write-Host 'successful:' $p; Start-Sleep -Seconds 1 } else { Write-Host 'not blocked:' $p; Start-Sleep -Seconds 1 } } } catch { Write-Host 'error while unblocking:' $_.Exception.Message; Start-Sleep -Seconds 1; exit 1 }"
 
 echo 1 > "%FLAG%"
 
 echo.
-echo    starting...
+echo starting...
 timeout /t 2 /nobreak >nul
 
 :FASTMODE
